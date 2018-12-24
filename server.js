a6922208b41374222c8216314e3dc9ad32534d03
Miguel Rodriguez <miguelantonio_rodriguez@ucol.mx>
2018-12-23 18:17:37 -0600

    Home, about, help pages updated

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 
 const port = process.env.PORT || 3000;
 
-hbs.registerPartials(__dirname + '/views/partials/')
+hbs.registerPartials(__dirname + '/views/partials/');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
@@ -21,15 +21,12 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-    res.send({
-        name: 'mike',
-        age: '19'
-    });
+    res.render('home.hbs');
 });
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
-        pageTitle: 'Testing',
+        pageTitle: 'About me',
         mrsName: 'Mike'
     });
 });
