b8ccb544d196539f374fef8006df2f7aca9c8496
Miguel Rodriguez <miguelantonio_rodriguez@ucol.mx>
2019-01-17 16:45:02 -0600

    Version update

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var app = express();
 
 const port = process.env.PORT || 3000;
 
+app.engine('hbs', hbs.__express);
 hbs.registerPartials(__dirname + '/views/partials/');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
@@ -12,6 +13,7 @@ app.use(express.static(__dirname + '/public'));
 app.use((req, res, next) => {
     var now = new Date().toString();
     var log = `${now}: ${req.method}, Url: ${req.url}`;
+    console.log(req);
 
     fs.appendFile('server.log', log + '\n', err => {
         if(err)console.log('Unable to register the log');
